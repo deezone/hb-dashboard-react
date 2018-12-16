6241628afcd296bb95f9f0c2e180443c3073ed18
deezone <darrendouglas.lee@gmail.com>
2018-12-15 22:58:52 -0500

    attempt at working with styles on App component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World!</h1>
+          <div className="Title">HydroBytes Dashboard</div>
+          <div className="Body">Data</div>
       </div>
     );
   }
